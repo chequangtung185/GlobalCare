e18a50942d856806cf200904dc427e42a0b33191
DuyenHo24 <duyen.24.07.dh@gmail.com>
2022-03-31 21:26:22 +0700

    update login without API

--- src/components/Router/AppRouter.js
+++ src/components/Router/AppRouter.js
@@ -2,7 +2,8 @@ import { Router } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../authenScreen/register";
 import Login from "../authenScreen/login";
-
+import Home from "../home/home";
+import ReportDemo from "../reportDemo/reportDemo";
 
 export default function AppRouter(){
     return(
@@ -10,10 +11,12 @@ export default function AppRouter(){
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/home/reportdemo" element={<ReportDemo />} />
             </Routes>
         </BrowserRouter>
-        
-
     )
 }
+
+
 
---
+++ src/components/home/home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+    Link,
+    useNavigate
+  } from "react-router-dom";
+function Home(){
+    let navigate = useNavigate();
+    const Logout =() => {
+        navigate("/")
+    };
+    return (
+        <>
+            <h2>Welcome to HomePage</h2>
+
+            <div>
+                <button onClick={Logout}>Logout</button>
+            </div>
+
+            <Link to={"/home/reportdemo"}>Chuyen trang report</Link>
+
+        </>
+    );
+};
+
+export default Home;
+
+
+
+
+
